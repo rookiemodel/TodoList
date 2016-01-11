c412504329e87fbe1a933b6859611062a5bbee6f
rookiemodel <29032510github>
2016-01-12 08:36:14 +0900

    add plugins

--- src/js/config.js
+++ src/js/config.js
@@ -1,11 +1,13 @@
 require.config({
-    baseUrl: "js/todolist/",
+    urlArgs: 'v=' +  (new Date()).getTime(),
+    baseUrl: 'js/todolist/',
     paths: {
         jquery: '../vendor/jquery-1.11.0.min',
         underscore: '../vendor/underscore-1.8.3.min',
         backbone: '../vendor/backbone-1.2.3.min',
         storage: '../vendor/backbone.localStorage-1.1.16.min',
-        text: '../vendor/text'
+        text: '../vendor/text',
+        plugins: '../vendor/plugins'
     },
     shim: {
         underscore: {
@@ -15,14 +17,19 @@ require.config({
             deps: ['underscore', 'jquery'],
             exports: 'Backbone'
         },
-        backboneLocalstorage: {
+        storage: {
             deps: ['backbone'],
-            exports: 'Store'
+            exports: 'Storage'
+        },
+        plugins: {
+            deps: ['jquery']
         }
     }
 });
 
-require(['Application'], function( Application ) {
+require(['plugins', 'Application'], function( Plugins, Application ) {
+    Platform.division();
+
     window.App = new Application();
     App.start();
 });
